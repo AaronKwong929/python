e63b51f19830b93bd8197a448af00f4ff662036a
AaronKwong929 <aaronkwong929@gmail.com>
2018-09-11 09:34:34 +0800

    .

--- CSVtexr.py
+++
@@ -1,6 +0,0 @@
-import csv
-f = open('C://Users/Arron/Desktop/csvtest.csv', 'w+')
-writer = csv.writer(f)
-writer.writerow(('id', 'name'))
-writer.writerow(('1', 'li'))
-writer.writerow(('2', 'wong'))
