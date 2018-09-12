7e567125a036e37d4930db9a78a45b84a7e5160e
AaronKwong929 <aaronkwong929@gmail.com>
2018-09-12 16:58:31 +0800

    1

--- 乘法口诀表.py
+++
@@ -1,9 +0,0 @@
-'''#单一排
-for i in range(0,10):
-  j=1
-  print(i,"*",j,"=",i*j)'''
-# 全表
-for i in range(1, 10, 1):
-    for j in range(1, i + 1, 1):
-        print(i, "*", j, "=", i * j, end="   ")
-    print("\n")
