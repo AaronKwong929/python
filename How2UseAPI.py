8127d2e0ab502cbd32cad644172143ab5deca342
AaronKwong929 <aaronkwong929@gmail.com>
2018-09-18 09:01:29 +0800

    API简单使用

--- How2UseAPI.py
+++ How2UseAPI.py
@@ -1,13 +1,11 @@
 import requests
-
 url = 'https://api.github.com/search/repositories?q=language:python&sort=start'
-
 r = requests.get(url)  # 返回请求状态
-
 print('status_code:', r.status_code)
 response_dict = r.json()
 print(response_dict.keys())  # 返回api内的关键字
-print('\ntotal repositories:', response_dict['total_count'])  # 返回total_count关键字内容
+print('\ntotal repositories:',
+      response_dict['total_count'])  # 返回total_count关键字内容
 repo_dicts = response_dict['items']
 print('repositories_return:', len(repo_dicts))  # 返回字典长度
 repo_dict = repo_dicts[0]  # 取字典内第一项
