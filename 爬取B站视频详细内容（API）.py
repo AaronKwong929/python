2c13260e4a75032f4fb8ae9a562af47679a83417
AaronKwong929 <aaronkwong929@gmail.com>
2018-10-08 15:57:21 +0800

    增加headers可以解决爬取不到json文件的问题（ expecting value lin1 column 1

--- 爬取B站视频详细内容（API）.py
+++ 爬取B站视频详细内容（API）.py
@@ -1,5 +1,9 @@
 import time
 import requests
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}
 # 难点在于找api
 # 打开详细视频页面 F12 - network - F5 - XHR - response - 找需要的内容 
 # 获取到api网址https://api.bilibili.com/x/web-interface/view?aid=32127626 验证之
@@ -8,7 +12,7 @@ import requests
 av = input('请输入视频AV号后的数字：')
 url = 'https://api.bilibili.com/x/web-interface/archive/stat?aid={}'.format(str(av))
 time.sleep(2)
-r = requests.get(url)
+r = requests.get(url, headers=headers)
 response_dict = r.json()
 print('视频av号：' + 'av' + str(av),
       '  播放量：', response_dict['data']['view'], 
