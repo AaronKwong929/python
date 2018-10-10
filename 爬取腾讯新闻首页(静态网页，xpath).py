81d028ab1cdd1598ee4b1e80792f1d431b2066f8
AaronKwong929 <aaronkwong929@gmail.com>
2018-10-10 14:12:12 +0800

    静态网页xpath练手，更新大标签对列表影响的问题

--- 爬取腾讯新闻首页(静态网页，xpath).py
+++ 爬取腾讯新闻首页(静态网页，xpath).py
@@ -10,9 +10,18 @@ url = 'http://www.qq.com/'
 
 html = requests.get(url, headers=headers)
 selector = etree.HTML(html.text)
+# 以下是将大标签一直写到a标签之前，每一个新闻标题作为一个单独的列表存在，用[0]来输出新闻标题
+infos = selector.xpath('//*[@id="newsInfoQuanguo"]/div/div/ul/li')
+for info in infos:
+    name = info.xpath('a/text()')[0]
+    print(name)
+    time.sleep(1)
+  # 以下是将所有新闻标题放在一个列表内，不用[0]来控制列表,再做一个for循环输出，时间复杂度高一点    
+'''
 infos = selector.xpath('//*[@id="newsInfoQuanguo"]')
 for info in infos:
     names = info.xpath('div/div/ul/li/a/text()')
     for name in names:
         print(name)
-        time.sleep(1)
+'''
+    
