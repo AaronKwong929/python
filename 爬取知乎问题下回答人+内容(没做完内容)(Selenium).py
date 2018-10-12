33e4472a50d14c8569ff715bba8e21ccd64bf586
AaronKwong929 <aaronkwong929@gmail.com>
2018-10-13 00:19:36 +0800

    10.13  0:18目前只能爬第一个人的答案，排版思考中

--- 爬取知乎问题下回答人+内容(没做完内容)(Selenium).py
+++ 爬取知乎问题下回答人+内容(没做完内容)(Selenium).py
@@ -8,13 +8,18 @@ browser.implicitly_wait(10)
 title = browser.find_element_by_xpath('//*[@id="root"]/div/main/div/div[1]/div[2]/div[1]/div[1]/h1').text
 browser.find_element_by_xpath('//*[@id="root"]/div/main/div/div[1]/div[2]/div[1]/div[1]/div[2]/div/div/div/button').click()
 brief = browser.find_element_by_xpath('//*[@id="root"]/div/main/div/div[1]/div[2]/div[1]/div[1]/div[2]/div/div/div/span').text
+follower = browser.find_element_by_xpath('//*[@id="root"]/div/main/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/strong').text
+read = browser.find_element_by_xpath('//*[@id="root"]/div/main/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[2]/div/strong').text
+'''
 answerers = browser.find_elements_by_class_name('UserLink-link')
-print('问题：' + title + '\n简介：' + brief + '\n回答者：')
+print('问题：' + title + '\n简介：' + brief + '\n关注者：' + follower + '\n被浏览：' + read + '\n回答者：' )
 for answerer in answerers:
-    print(answerer.text)
-# answers = browser.find_elements_by_class_name('RichText ztext CopyrightRichText-richText')
+    print(answerer.text)'''
+answers = browser.find_elements_by_xpath('//*[@id="QuestionAnswers-answers"]/div/div/div[2]/div/div[1]/div/div[2]/div[1]/span/p')
 '''print('问题：' + title + '\n简介：' + brief + '\n回答者：')
 for answerer, answer in zip(answerers, answers):
     print(answerer.text + '  ' + answer.text)
 '''
+for answer in answers:
+    print(answer.text)
 browser.quit()
