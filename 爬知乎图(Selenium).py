840e6cbb4c3ffb625f1079d7e58e073776502595
AaronKwong929 <aaronkwong929@gmail.com>
2018-10-14 22:51:32 +0800

    待做：循环滚动页面进行爬取，判断是否重复

--- 爬知乎图(Selenium).py
+++ 爬知乎图(Selenium).py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import urllib.request
 import time
 browser = webdriver.Chrome()
 browser.maximize_window()
@@ -10,8 +11,11 @@ browser.execute_script(js)
 browser.implicitly_wait(20)
 time.sleep(20)
 
-img_set = browser.find_element_by_xpath('//*[@id="QuestionAnswers-answers"]/div/div/div[2]/div/div[1]/div/div[2]/div[1]/span/figure/img[@src]')
+img_set = browser.find_element_by_xpath('//*[@id="QuestionAnswers-answers"]/div/div/div[2]/div/div[1]/div/div[2]/div[1]/span/figure/img')
+print(img_set.get_attribute('src'))
+img_path = img_set.get_attribute('src')
 
-
-
+f = open("C:/Users/Arron/Desktop/photo/" + str('test')+".jpg", 'wb')
+f.write((urllib.request.urlopen(img_path)).read())
+f.close()
 browser.quit()
