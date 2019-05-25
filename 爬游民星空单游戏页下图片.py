663bef44b68c72b9bd95880205eee369dae12251
AaronKwong929 <AaronKwong929@gmail.com>
2019-05-25 12:37:43 +0800

    多网页 + 游戏分类图片

--- 爬游民星空单游戏页下图片.py
+++ 爬游民星空单游戏页下图片.py
@@ -2,26 +2,34 @@ import requests
 from lxml import etree
 import urllib.request
 
-src_links = []
 headers = {
     'User-Agent':
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 }
-url = 'https://down.gamersky.com/pc/201411/486874.shtml'
 
-html = requests.get(url, headers=headers)
-selector = etree.HTML(html.text)
+src_links = []
 
-infos = selector.xpath('///div[5]/ul/li/img/@src')
+urls = [
+    'https://down.gamersky.com/pc/201411/486874.shtml',
+    'https://down.gamersky.com/pc/201604/739324.shtml'
+]
 
-for info in infos:
-    src_links.append(info)
 
-# 文件写入
-count = 1
-for src_link in src_links:
-    f = open("C:/Users/Arron/Desktop/photo/" + str(count) + ".jpg", 'wb')
-    f.write((urllib.request.urlopen(src_link)).read())
-    f.close()
-    print('第' + str(count) + '张图片已爬取。')
-    count += 1
+for url in urls:
+    count = 1
+    shortName = url.split('https://down.gamersky.com/pc/')[1]
+    html = requests.get(url, headers=headers)
+    html.encoding = "utf-8"  # 乱码解决核心
+    selector = etree.HTML(html.text)
+    infos = selector.xpath('//div[5]/ul/li/img/@src')
+    name = selector.xpath('//div[@class="tit"]/text()')[0]
+    for info in infos:
+        src_links.append(info)
+    for src_link in src_links:
+        f = open("C:/Users/Arron/Desktop/photo/" + name + str(count) + ".jpg", 'wb')
+        f.write((urllib.request.urlopen(src_link)).read())
+        f.close()
+        print(name + '第' + str(count) + '张图片已爬取。')
+        count += 1
+    src_links = []
+    count = 0
