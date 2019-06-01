2e7adf126d4a9af1ae063dd1460f6a24d7f3e1a8
AaronKwong929 <AaronKwong929@gmail.com>
2019-06-01 21:58:57 +0800

    加爬个封面

--- 爬游民星空单游戏页下图片(xpath).py
+++ 爬游民星空单游戏页下图片(xpath).py
@@ -21,10 +21,13 @@ for url in urls:
     html = requests.get(url, headers=headers)
     html.encoding = "utf-8"  # 乱码解决核心
     selector = etree.HTML(html.text)
+    cover = selector.xpath('//div[2]/div[1]/img/@src')[0]
     infos = selector.xpath('//div[5]/ul/li/img/@src')
     name = selector.xpath('//div[@class="tit"]/text()')[0]
     for info in infos:
         src_links.append(info)
+    open("C:/Users/Arron/Desktop/photo/" + name + "-cover.jpg", 'wb').write((urllib.request.urlopen(cover)).read())
+    print(name + '-封面已爬取')
     for src_link in src_links:
         f = open("C:/Users/Arron/Desktop/photo/" + name + str(count) + ".jpg", 'wb')
         f.write((urllib.request.urlopen(src_link)).read())
